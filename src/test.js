8150e726b918f510f6afdf88aeeedf877a018a5a
patsakornsit <159615564+patsakornsit@users.noreply.github.com>
2024-02-11 01:14:51 +0700

    test.js

--- src/test.js
+++ src/test.js
@@ -131,7 +131,7 @@ const TableToCardComponent = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:5009/for');
+        const response = await fetch('http://127.0.0.1:5000/for');
         if (response.ok) {
           const responseData = await response.json();
           console.log('Fetched data:', responseData);
